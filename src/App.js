d09017b9081f6be4ddad7351e87d81f8776e0290
revyrob <kayle.robson@gmail.com>
2023-03-25 11:58:06 -0700

    update the app.js and the solar system scss file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
-import ExplorePlanets from "./components/explorePlanets";
+import SolarSystem from "./components/solarSystem/solarSystem";
 
 function App() {
   return (
     <div className="App">
-      <ExplorePlanets />
+      <SolarSystem />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/components/solarSystem/solarSystem.js
+++ src/components/solarSystem/solarSystem.js
@@ -1,16 +1,15 @@
-import React from 'react'
-import './SolarSystem.scss';
+import React from "react";
+import "./solarSystem.scss";
 
 function SolarSystem() {
-    return (
-        <div className='solarSystem'>
-            <div className='sun'></div>
-            <div className='orbit__mecury'>
-                <div className='planet__mecury'></div>
-            </div>
-        
-        </div>
-        )
-    }
-    
-    export default SolarSystem
+  return (
+    <div className="solarSystem">
+      <div className="sun"></div>
+      <div className="orbit__mecury">
+        <div className="planet__mecury"></div>
+      </div>
+    </div>
+  );
+}
+
+export default SolarSystem;
