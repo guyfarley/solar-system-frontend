932939e10aec27cd13b02006bdec453d48ecd7e8
revyrob <kayle.robson@gmail.com>
2023-03-26 01:03:17 -0700

    made the planets in 2d

--- src/components/solarSystem/solarSystem.js
+++ src/components/solarSystem/solarSystem.js
@@ -3,60 +3,62 @@ import "./solarSystem.scss";
 
 function SolarSystem() {
   return (
-    <div className="solarSystem">
-      <div className="sun"></div>
+    <div className="universe">
+      <div className="solarSystem">
+        <div className="sun"></div>
 
-      <div className="orbit__mecury">
-        <div className="orbit">
-          <div className="planet__mecury"></div>
+        <div className="orbit orbit__mecury">
+          <div className="orbit__inner">
+            <div className="planet__mecury"></div>
+          </div>
         </div>
-      </div>
 
-      <div className="orbit__venus">
-        <div className="orbit">
-          <div className="planet__venus"></div>
+        <div className="orbit orbit__venus">
+          <div className="orbit__inner">
+            <div className="planet__venus"></div>
+          </div>
         </div>
-      </div>
 
-      <div className="orbit__earth">
-        <div className="orbit">
-          <div className="planet__earth"></div>
+        <div className="orbit orbit__earth">
+          <div className="orbit__inner">
+            <div className="planet__earth"></div>
+          </div>
         </div>
-      </div>
 
-      <div className="orbit__mars">
-        <div className="orbit">
-          <div className="planet__mars"></div>
+        <div className="orbit orbit__mars">
+          <div className="orbit__innerQuarter">
+            <div className="planet__mars"></div>
+          </div>
         </div>
-      </div>
 
-      <div className="orbit__jupiter">
-        <div className="orbit">
-          <div className="planet__jupiter"></div>
+        <div className="orbit orbit__jupiter">
+          <div className="orbit__innerMid">
+            <div className="planet__jupiter"></div>
+          </div>
         </div>
-      </div>
 
-      <div className="orbit__saturn">
-        <div className="orbit">
-          <div className="planet__saturn"></div>
+        <div className="orbit orbit__saturn">
+          <div className="orbit__innerMid">
+            <div className="planet__saturn"></div>
+          </div>
         </div>
-      </div>
 
-      <div className="orbit__uranus">
-        <div className="orbit">
-          <div className="planet__uranus"></div>
+        <div className="orbit orbit__uranus">
+          <div className="orbit__innerQuarter">
+            <div className="planet__uranus"></div>
+          </div>
         </div>
-      </div>
 
-      <div className="orbit__neptune">
-        <div className="orbit">
-          <div className="planet__neptune"></div>
+        <div className="orbit orbit__neptune">
+          <div className="orbit__innerQuarter">
+            <div className="planet__neptune"></div>
+          </div>
         </div>
-      </div>
 
-      <div className="orbit__pluto">
-        <div className="orbit">
-          <div className="planet__pluto"></div>
+        <div className="orbit orbit__pluto">
+          <div className="orbit__innerPluto">
+            <div className="planet__pluto"></div>
+          </div>
         </div>
       </div>
     </div>
