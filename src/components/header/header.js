0d86b71db05a6f98a0449a82b7e7c93f4393542c
revyrob <kayle.robson@gmail.com>
2023-03-26 11:45:06 -0700

    adding star background with overlay to planets and added colors to navbar

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,22 +1,21 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
-import RocketLaunchIcon from '@mui/icons-material/RocketLaunch';
-import { Link } from 'react-router-dom';
-import './header.scss';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import RocketLaunchIcon from "@mui/icons-material/RocketLaunch";
+import { Link } from "react-router-dom";
+import "./header.scss";
 // import Avatar from '@mui/material/Avatar';
 // import Tooltip from '@mui/material/Tooltip';
 
 const Header = () => {
-
   const [anchorElNav, setAnchorElNav] = React.useState(null);
 
   const handleOpenNavMenu = (event) => {
@@ -28,10 +27,12 @@ const Header = () => {
   };
 
   return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
+    <AppBar position="static" className="header">
+      <Container maxWidth="xl" className="header__container">
         <Toolbar disableGutters>
-          <RocketLaunchIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 3 }} />
+          <RocketLaunchIcon
+            sx={{ display: { xs: "none", md: "flex" }, mr: 3 }}
+          />
           {/* <Typography
             variant="h6"
             noWrap
@@ -50,7 +51,7 @@ const Header = () => {
             LOGO
           </Typography> */}
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
               aria-label="account of current user"
@@ -65,38 +66,48 @@ const Header = () => {
               id="menu-appbar"
               anchorEl={anchorElNav}
               anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
+                vertical: "bottom",
+                horizontal: "left",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
+                vertical: "top",
+                horizontal: "left",
               }}
               open={Boolean(anchorElNav)}
               onClose={handleCloseNavMenu}
               sx={{
-                display: { xs: 'block', md: 'none' },
+                display: { xs: "block", md: "none" },
               }}
             >
               <MenuItem onClick={handleCloseNavMenu}>
-                <Link to="/#explore-planets"><Typography textAlign="center">Planets</Typography></Link>
+                <Link to="/#explore-planets">
+                  <Typography textAlign="center">Planets</Typography>
+                </Link>
               </MenuItem>
 
               <MenuItem onClick={handleCloseNavMenu}>
-                <Link to="/#resources"><Typography textAlign="center">Resources</Typography></Link>
+                <Link to="/#resources">
+                  <Typography textAlign="center">Resources</Typography>
+                </Link>
               </MenuItem>
 
               <MenuItem onClick={handleCloseNavMenu}>
-                <Link to="/#fun-facts"><Typography textAlign="center">Fun Facts</Typography></Link>
+                <Link to="/#fun-facts">
+                  <Typography textAlign="center">Fun Facts</Typography>
+                </Link>
               </MenuItem>
 
               <MenuItem onClick={handleCloseNavMenu}>
-                <Link to="/#about-us"><Typography textAlign="center">About Us</Typography></Link>
+                <Link to="/#about-us">
+                  <Typography textAlign="center">About Us</Typography>
+                </Link>
               </MenuItem>
             </Menu>
           </Box>
-          <RocketLaunchIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <RocketLaunchIcon
+            sx={{ display: { xs: "flex", md: "none" }, mr: 1 }}
+          />
           {/* <Typography
             variant="h5"
             noWrap
@@ -115,33 +126,44 @@ const Header = () => {
           >
             LOGO
           </Typography> */}
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             <Button
               onClick={handleCloseNavMenu}
-              sx={{ my: 2, mr: 3, color: 'black', display: 'block' }}
-            ><Link to="/#explore-planets"><Typography textAlign="center">Planets</Typography></Link>
+              sx={{ my: 2, mr: 3, color: "black", display: "block" }}
+            >
+              <Link to="/#explore-planets">
+                <Typography textAlign="center">Planets</Typography>
+              </Link>
             </Button>
             <Button
               onClick={handleCloseNavMenu}
-              sx={{ my: 2, mr: 3, color: 'black', display: 'block' }}
-            ><Link to="/#resources"><Typography textAlign="center">Resources</Typography></Link>
+              sx={{ my: 2, mr: 3, color: "black", display: "block" }}
+            >
+              <Link to="/#resources">
+                <Typography textAlign="center">Resources</Typography>
+              </Link>
             </Button>
             <Button
               onClick={handleCloseNavMenu}
-              sx={{ my: 2, mr: 3, color: 'black', display: 'block' }}
-            ><Link to="/#fun-facts"><Typography textAlign="center">Fun Facts</Typography></Link>
+              sx={{ my: 2, mr: 3, color: "black", display: "block" }}
+            >
+              <Link to="/#fun-facts">
+                <Typography textAlign="center">Fun Facts</Typography>
+              </Link>
             </Button>
             <Button
               onClick={handleCloseNavMenu}
-              sx={{ my: 2, mr: 3, color: 'black', display: 'block' }}
-            ><Link to="/#about-us"><Typography textAlign="center">About Us</Typography></Link>
+              sx={{ my: 2, mr: 3, color: "black", display: "block" }}
+            >
+              <Link to="/#about-us">
+                <Typography textAlign="center">About Us</Typography>
+              </Link>
             </Button>
           </Box>
-
         </Toolbar>
       </Container>
     </AppBar>
   );
-}
+};
 
 export default Header;
