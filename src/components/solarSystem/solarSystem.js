dcdb0f4b32e7675402c8f22af715dea5526e5180
revyrob <kayle.robson@gmail.com>
2023-03-25 23:44:09 -0700

    added planets in flat plane, working on distances from the sun

--- src/components/solarSystem/solarSystem.js
+++ src/components/solarSystem/solarSystem.js
@@ -5,8 +5,59 @@ function SolarSystem() {
   return (
     <div className="solarSystem">
       <div className="sun"></div>
+
       <div className="orbit__mecury">
-        <div className="planet__mecury"></div>
+        <div className="orbit">
+          <div className="planet__mecury"></div>
+        </div>
+      </div>
+
+      <div className="orbit__venus">
+        <div className="orbit">
+          <div className="planet__venus"></div>
+        </div>
+      </div>
+
+      <div className="orbit__earth">
+        <div className="orbit">
+          <div className="planet__earth"></div>
+        </div>
+      </div>
+
+      <div className="orbit__mars">
+        <div className="orbit">
+          <div className="planet__mars"></div>
+        </div>
+      </div>
+
+      <div className="orbit__jupiter">
+        <div className="orbit">
+          <div className="planet__jupiter"></div>
+        </div>
+      </div>
+
+      <div className="orbit__saturn">
+        <div className="orbit">
+          <div className="planet__saturn"></div>
+        </div>
+      </div>
+
+      <div className="orbit__uranus">
+        <div className="orbit">
+          <div className="planet__uranus"></div>
+        </div>
+      </div>
+
+      <div className="orbit__neptune">
+        <div className="orbit">
+          <div className="planet__neptune"></div>
+        </div>
+      </div>
+
+      <div className="orbit__pluto">
+        <div className="orbit">
+          <div className="planet__pluto"></div>
+        </div>
       </div>
     </div>
   );
