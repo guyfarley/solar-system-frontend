645115e63b9c62199c386a7d78c0a3fe825cc4bf
Guy Farley <guy.e.farley@gmail.com>
2023-03-25 19:59:07 -0700

    material ui header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,147 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import RocketLaunchIcon from '@mui/icons-material/RocketLaunch';
+import { Link } from 'react-router-dom';
+import './header.scss';
+// import Avatar from '@mui/material/Avatar';
+// import Tooltip from '@mui/material/Tooltip';
+
 const Header = () => {
 
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
   return (
-    <div>
-      <p>header</p>
-      {/* logo */}
-      {/* nav bar: planets, resources, fun facts, team */}
-    </div>
-  )
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <RocketLaunchIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 3 }} />
+          {/* <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 3,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography> */}
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              <MenuItem onClick={handleCloseNavMenu}>
+                <Link to="/#explore-planets"><Typography textAlign="center">Planets</Typography></Link>
+              </MenuItem>
+
+              <MenuItem onClick={handleCloseNavMenu}>
+                <Link to="/#resources"><Typography textAlign="center">Resources</Typography></Link>
+              </MenuItem>
+
+              <MenuItem onClick={handleCloseNavMenu}>
+                <Link to="/#fun-facts"><Typography textAlign="center">Fun Facts</Typography></Link>
+              </MenuItem>
+
+              <MenuItem onClick={handleCloseNavMenu}>
+                <Link to="/#about-us"><Typography textAlign="center">About Us</Typography></Link>
+              </MenuItem>
+            </Menu>
+          </Box>
+          <RocketLaunchIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          {/* <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography> */}
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            <Button
+              onClick={handleCloseNavMenu}
+              sx={{ my: 2, mr: 3, color: 'black', display: 'block' }}
+            ><Link to="/#explore-planets"><Typography textAlign="center">Planets</Typography></Link>
+            </Button>
+            <Button
+              onClick={handleCloseNavMenu}
+              sx={{ my: 2, mr: 3, color: 'black', display: 'block' }}
+            ><Link to="/#resources"><Typography textAlign="center">Resources</Typography></Link>
+            </Button>
+            <Button
+              onClick={handleCloseNavMenu}
+              sx={{ my: 2, mr: 3, color: 'black', display: 'block' }}
+            ><Link to="/#fun-facts"><Typography textAlign="center">Fun Facts</Typography></Link>
+            </Button>
+            <Button
+              onClick={handleCloseNavMenu}
+              sx={{ my: 2, mr: 3, color: 'black', display: 'block' }}
+            ><Link to="/#about-us"><Typography textAlign="center">About Us</Typography></Link>
+            </Button>
+          </Box>
+
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 }
 
 export default Header;
