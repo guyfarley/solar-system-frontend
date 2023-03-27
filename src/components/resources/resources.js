e090bbc2d58df5950650ebcb450b1542db3ad138
revyrob <kayle.robson@gmail.com>
2023-03-27 08:38:20 -0700

    worked on resource section

--- src/components/resources/resources.js
+++ src/components/resources/resources.js
@@ -1,4 +1,9 @@
 import "./resources.scss";
+import React from "react";
+import nasa from "../../assets/images/nasa.png";
+import esa from "../../assets/images/euro.png";
+import solarRap from "../../assets/images/solarRap.png";
+import nationalGeo from "../../assets/images/vidnational.png";
 
 const Resources = () => {
   return (
@@ -8,6 +13,32 @@ const Resources = () => {
         <h3 className="resources__subtitle">
           Dive into the resources to continue your learning journey
         </h3>
+      </div>
+
+      <div className="resources__div-wrapper">
+        <div className="resources__div1">
+          {" "}
+          <div className="resourceCard">
+            <img className="resourceCard__img" src={nasa} alt={nasa} />
+          </div>
+          <div className="resourceCard">
+            <img className="resourceCard__img" src={esa} alt={esa} />
+          </div>
+        </div>
+
+        <div className="resources__div2">
+          {" "}
+          <div className="resourceCard">
+            <img className="resourceCard__img" src={solarRap} alt={solarRap} />
+          </div>
+          <div className="resourceCard">
+            <img
+              className="resourceCard__img"
+              src={nationalGeo}
+              alt={nationalGeo}
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
