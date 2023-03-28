4bc27801790f20e2fdc01bf3d6114b5fe4271533
revyrob <kayle.robson@gmail.com>
2023-03-27 21:33:16 -0700

    added cards

--- src/components/funFacts/funFacts.js
+++ src/components/funFacts/funFacts.js
@@ -1,9 +1,122 @@
+import React from "react";
+import "./funFacts.scss";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Button, CardActionArea, CardActions } from "@mui/material";
+import planet from "../../assets/images/david-menidrey.jpg";
+
 const FunFacts = () => {
   return (
-    <div>
-      <p>fun facts!</p>
-    </div>
-  )
-}
+    <section className="funFacts">
+      <div className="funFacts__header">
+        <h1 className="funFacts__title">Fun Facts</h1>
+        <h3 className="funFacts__subtitle">About the Solar System</h3>
+      </div>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image={planet}
+            alt="david menidrey planet photo unsplash"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              What is a Planet?
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              <p>1. It must orbit a star.</p>
+              <p>
+                2. It must be big enough to have enough gravity to force it into
+                a spherical shape.
+              </p>
+              <p>
+                3. It must be big enough that its gravity cleared away any other
+                objects of a similar size near its orbit around the Sun.
+              </p>
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <a href="https://solarsystem.nasa.gov/planets/in-depth/">
+            <Button size="small" color="primary">
+              Resource
+            </Button>
+          </a>
+        </CardActions>
+      </Card>
+
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image={planet}
+            alt="david menidrey planet photo unsplash"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              What is a Planet?
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              <p>1. It must orbit a star.</p>
+              <p>
+                2. It must be big enough to have enough gravity to force it into
+                a spherical shape.
+              </p>
+              <p>
+                3. It must be big enough that its gravity cleared away any other
+                objects of a similar size near its orbit around the Sun.
+              </p>
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <a href="https://solarsystem.nasa.gov/planets/in-depth/">
+            <Button size="small" color="primary">
+              Resource
+            </Button>
+          </a>
+        </CardActions>
+      </Card>
+
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image={planet}
+            alt="david menidrey planet photo unsplash"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              What is a Planet?
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              <p>1. It must orbit a star.</p>
+              <p>
+                2. It must be big enough to have enough gravity to force it into
+                a spherical shape.
+              </p>
+              <p>
+                3. It must be big enough that its gravity cleared away any other
+                objects of a similar size near its orbit around the Sun.
+              </p>
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <a href="https://solarsystem.nasa.gov/planets/in-depth/">
+            <Button size="small" color="primary">
+              Resource
+            </Button>
+          </a>
+        </CardActions>
+      </Card>
+    </section>
+  );
+};
 
 export default FunFacts;
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -2,9 +2,9 @@ import SolarSystem from "../../components/solarSystem/solarSystem";
 import Header from "../../components/header/header.js";
 import ExplorePlanets from "../../components/explorePlanets/explorePlanets";
 import Resources from "../../components/resources/resources";
+import FunFacts from "../../components/funFacts/funFacts";
 
 const Home = () => {
-
   // ping the server to query database for all planet data - save to variable
 
   return (
@@ -17,7 +17,9 @@ const Home = () => {
       <section id="resources">
         <Resources />
       </section>
-      <section id="fun-facts"></section>
+      <section id="fun-facts">
+        <FunFacts />
+      </section>
       <section id="about-us"></section>
     </div>
   );
