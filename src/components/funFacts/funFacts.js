340a96147315533a5829dd2b28c35684a98d09b3
revyrob <kayle.robson@gmail.com>
2023-03-27 22:23:07 -0700

    added fun facts, new images, and styling for mobile to desktop

--- src/components/funFacts/funFacts.js
+++ src/components/funFacts/funFacts.js
@@ -6,6 +6,8 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { Button, CardActionArea, CardActions } from "@mui/material";
 import planet from "../../assets/images/david-menidrey.jpg";
+import charon from "../../assets/images/charon.jpg";
+import uranus from "../../assets/images/uranus.jpg";
 
 const FunFacts = () => {
   return (
@@ -15,7 +17,7 @@ const FunFacts = () => {
         <h3 className="funFacts__subtitle">About the Solar System</h3>
       </div>
       <div className="funFacts__card-wrapper">
-        <Card sx={{ maxWidth: 345 }}>
+        <Card className="funFacts__card" sx={{ maxWidth: 345 }}>
           <CardActionArea>
             <CardMedia
               component="img"
@@ -41,78 +43,81 @@ const FunFacts = () => {
             </CardContent>
           </CardActionArea>
           <CardActions>
-            <a href="https://solarsystem.nasa.gov/planets/in-depth/">
-              <Button size="small" color="primary">
-                Resource
-              </Button>
+            <a
+              href="https://solarsystem.nasa.gov/planets/in-depth/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <button className="funFacts__btn-bot">Resource</button>
             </a>
           </CardActions>
         </Card>
 
-        <Card sx={{ maxWidth: 345 }}>
+        <Card className="funFacts__card" sx={{ maxWidth: 345 }}>
           <CardActionArea>
             <CardMedia
               component="img"
               height="140"
-              image={planet}
-              alt="david menidrey planet photo unsplash"
+              image={charon}
+              alt="NASA's New Horizons capture or Charon"
             />
             <CardContent>
               <Typography gutterBottom variant="h5" component="div">
-                What is a Planet?
+                What is a double planetary system?
               </Typography>
               <Typography variant="body2" color="text.secondary">
-                <p>1. It must orbit a star.</p>
                 <p>
-                  2. It must be big enough to have enough gravity to force it
-                  into a spherical shape.
-                </p>
-                <p>
-                  3. It must be big enough that its gravity cleared away any
-                  other objects of a similar size near its orbit around the Sun.
+                  Both objects are planets, or planetary-mass objects, that
+                  orbit around a common center of mass, rather than one object
+                  orbiting around the other. Pluto and it's largest moon,
+                  Charon, are the only double planetary system in our solar
+                  system. The same surfaces of Charon and Pluto always face each
+                  other, a phenomenon called mutual tidal locking.
                 </p>
               </Typography>
             </CardContent>
           </CardActionArea>
           <CardActions>
-            <a href="https://solarsystem.nasa.gov/planets/in-depth/">
-              <Button size="small" color="primary">
-                Resource
-              </Button>
+            <a
+              href="https://solarsystem.nasa.gov/moons/pluto-moons/charon/in-depth/#:~:text=At%20half%20the%20size%20of,phenomenon%20called%20mutual%20tidal%20locking."
+              target="_blank"
+              rel="noreferrer"
+            >
+              <button className="funFacts__btn-bot">Resource</button>
             </a>
           </CardActions>
         </Card>
 
-        <Card sx={{ maxWidth: 345 }}>
+        <Card className="funFacts__card" sx={{ maxWidth: 345 }}>
           <CardActionArea>
             <CardMedia
               component="img"
               height="140"
-              image={planet}
-              alt="david menidrey planet photo unsplash"
+              image={uranus}
+              alt="planet uranus from hubble telescope"
             />
             <CardContent>
               <Typography gutterBottom variant="h5" component="div">
-                What is a Planet?
+                Planet on it's side for 21 Earth years!
               </Typography>
               <Typography variant="body2" color="text.secondary">
-                <p>1. It must orbit a star.</p>
                 <p>
-                  2. It must be big enough to have enough gravity to force it
-                  into a spherical shape.
-                </p>
-                <p>
-                  3. It must be big enough that its gravity cleared away any
-                  other objects of a similar size near its orbit around the Sun.
+                  Uranus has an unusual orientation, it is about 90 degrees
+                  sideways compared to other planets. This tilt causes the most
+                  extreme weather conditions because during half of Uranus' year
+                  (21 Earth years) the sun shines directly over the north or
+                  south pole of the planet.
                 </p>
               </Typography>
             </CardContent>
           </CardActionArea>
           <CardActions>
-            <a href="https://solarsystem.nasa.gov/planets/in-depth/">
-              <Button size="small" color="primary">
-                Resource
-              </Button>
+            <a
+              href="https://www.space.com/35695-weirdest-solar-system-facts.html"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <button className="funFacts__btn-bot">Resource</button>
             </a>
           </CardActions>
         </Card>
