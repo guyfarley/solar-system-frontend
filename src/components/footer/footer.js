6f1d47084504a499b4631557f384717eacf0e446
revyrob <kayle.robson@gmail.com>
2023-03-29 14:02:37 -0700

    added to footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,10 +1,25 @@
 const Footer = () => {
   return (
-    <div>
+    <section className="footer">
+      <div className="footer__wrapper">
+        <div className="footer__nav">
+          <a href="">
+            <h4>Planets</h4>
+          </a>
+          <a href="">
+            <h4>Fun Facts</h4>
+          </a>
+          <a href="">
+            <h4>Resources</h4>
+          </a>
+        </div>
+        <div className="footer__team"></div>
+        <div className="footer__copyright"></div>
+      </div>
       <p>footer</p>
       {/* nav bar: planets, resources, fun facts, team */}
-    </div>
-  )
-}
+    </section>
+  );
+};
 
 export default Footer;
