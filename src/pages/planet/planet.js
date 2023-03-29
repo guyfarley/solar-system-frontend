decf7a59ec4088c6bd404da42604d1b96c6257ee
Guy Farley <guy.e.farley@gmail.com>
2023-03-28 20:04:35 -0700

    fixed planet page rendering

--- src/pages/planet/planet.js
+++ src/pages/planet/planet.js
@@ -7,83 +7,20 @@ import { useParams } from "react-router-dom";
 import "./planet.scss";
 import { useState, useEffect } from "react";
 import axios from "axios";
+
 const Planet = () => {
-  // const URL = "http://localhost:8080/";
 
-  // const [currentPlanet, setCurrentPlanet] = useState([]);
-  // let planet;
-
-  // const getAllPlanets = async () => {
-  //   try {
-  //     const response = await axios.get(URL);
-  //     const planets = response.data.rows;
-  //     planet = planets.filter((planet) => planet.planet_id === planetSlug);
-  //     setCurrentPlanet(planet);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   getAllPlanets();
-  // }, []);
-
-  // const dummyPlanet = {
-  //   planet_id: 123,
-  //   planet_name: "Dummy Planet",
-  //   moons: [
-  //     {
-  //       id: 1.1,
-  //       name: "Moon 1",
-  //       nameHistory: "History of the name of this moon",
-  //       size: "Small",
-  //       category: "Moon",
-  //     },
-  //     {
-  //       id: 1.2,
-  //       name: "Moon 2",
-  //       nameHistory: "History of the name of this moon",
-  //       size: "Small",
-  //       category: "Moon",
-  //     },
-  //     {
-  //       id: 1.3,
-  //       name: "Moon 3",
-  //       nameHistory: "History of the name of this moon",
-  //       size: "Small",
-  //       category: "Moon",
-  //     },
-  //   ],
-  // };
-
-  // console.log(dummyPlanet.moons);
   const { planetSlug } = useParams();
   const URLplanet = `http://localhost:8080/planet/${planetSlug}`;
-  const URLplanets = `http://localhost:8080/`;
-  console.log(planetSlug);
-
-  //const URL = http://localhost:8080/planet/${planetSlug};
-
+  const [isLoading, setLoading] = useState(true);
   const [planetData, setPlanetData] = useState([]);
-  const [planetsData, setPlanetsData] = useState([]);
 
-  let planet;
-
-  const getPlanets = async () => {
+  const getPlanet = () => {
     try {
-      const response = await axios.get(URLplanets);
-      const planets = response.data.rows;
-      planet = planets.filter((planet) => planet.planet_id === planetSlug);
-      setPlanetsData(planet);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const getPlanet = async () => {
-    try {
-      const response = await axios.get(URLplanet);
-      setPlanetData(response.data.rows);
+      axios.get(URLplanet).then(response => {
+        setPlanetData(response.data.rows);
+        setLoading(false);
+      })
     } catch (error) {
       console.error(error);
     }
@@ -91,22 +28,19 @@ const Planet = () => {
 
   useEffect(() => {
     getPlanet();
-    getPlanets();
+    console.log("planet data from planet page: ", planetData);
   }, []);
 
-  //console.log("planet data from planet page: ", planetData);
-
-  // let planet = planetData.filter(planet => planet.planet_id === planetSlug);
-  // let currentPlanet = planet[0];
-
-  // console.log("current planet: ", currentPlanet);
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
 
   return (
     <>
       <Header />
       <div className="planet">
-        <PlanetBanner data={planetsData} />
-        <PlanetInfo data={planetsData} />
+        <PlanetBanner data={planetData} />
+        <PlanetInfo data={planetData} />
         <div className="planetheader">
           {/* <h1 className="planettitle">Moons</h1> */}
           <h1 className="planet__subtitle">Check out the moons</h1>
