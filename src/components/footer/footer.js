e5df9c2ae56274df03773297802b3a9a03007b76
revyrob <kayle.robson@gmail.com>
2023-03-29 19:28:10 -0700

    almost finished footer.  Need to work on color scheme and adding photos and links to circles

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,22 +1,43 @@
+import React from "react";
+import { HashLink as Link } from "react-router-hash-link";
+import "./footer.scss";
+import RocketLaunchIcon from "@mui/icons-material/RocketLaunch";
+
 const Footer = () => {
   return (
     <section className="footer">
       <div className="footer__wrapper">
         <div className="footer__nav">
-          <a href="#explore-planets">
-            <h4>Planets</h4>
-          </a>
-          <a href="#fun-facts">
-            <h4>Fun Facts</h4>
-          </a>
-          <a href="#resources">
-            <h4>Resources</h4>
-          </a>
+          <div className="footer__nav--title">
+            <Link smooth to="/#explore-planets">
+              <h4>Planets</h4>
+            </Link>
+            <Link smooth to="/#fun-facts">
+              <h4>Fun Facts</h4>
+            </Link>
+            <Link smooth to="/#resources">
+              <h4>Resources</h4>
+            </Link>
+          </div>
+          <div className="footer__nav--name">
+            <RocketLaunchIcon
+              sx={{
+                display: { xs: "none", md: "flex" },
+                mr: 1,
+                fill: "white",
+              }}
+            />
+            <h4>TEAM PLUTO</h4>
+          </div>
         </div>
-        <div className="footer__team"></div>
-        <div className="footer__copyright"></div>
+        <div className="footer__team">
+          <div className="footer__team--mem"></div>
+          <div className="footer__team--mem"></div>
+          <div className="footer__team--mem"></div>
+        </div>
       </div>
-      <p>footer</p>
+      <div className="footer__copyright">©️ Team Pluto 2023</div>
+
       {/* nav bar: planets, resources, fun facts, team */}
     </section>
   );
