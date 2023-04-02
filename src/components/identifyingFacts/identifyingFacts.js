241611b07d1ae50eaf59887f258acdf971d7dd06
revyrob <kayle.robson@gmail.com>
2023-04-01 17:40:09 -0700

    added some animation on the balls

--- src/components/identifyingFacts/identifyingFacts.js
+++ src/components/identifyingFacts/identifyingFacts.js
@@ -1,7 +1,11 @@
 import "./identifyingFacts.scss";
 import React from "react";
+import { useRef } from "react";
+import { useInView } from "framer-motion";
 
-const identifyingFacts = ({ data }) => {
+const IdentifyingFacts = ({ data }) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
   // Splitting facts into 3 separate variables
   let split_string = data[0].three_facts.split(".");
   const fact1 = split_string[0];
@@ -13,13 +17,40 @@ const identifyingFacts = ({ data }) => {
       <div className="identifying-facts__header">
         <div className="identifying-facts__header--title">{`Fun Facts About ${data[0].planet_name}!`}</div>
       </div>
-      <div className="identifying-facts__container">
-        <div className="identifying-facts__data1">{fact1}</div>
-        <div className="identifying-facts__data2">{fact2}</div>
-        <div className="identifying-facts__data3">{fact3}</div>
+      <div ref={ref} className="identifying-facts__container">
+        <div
+          style={{
+            transform: isInView ? "none" : "translateX(-200px)",
+            opacity: isInView ? 1 : 0,
+            transition: "all 0.9s cubic-bezier(.17,.67,.84,.25) 0.5s",
+          }}
+          className="identifying-facts__data1"
+        >
+          {fact1}
+        </div>
+        <div
+          style={{
+            transform: isInView ? "none" : "translateX(-200px)",
+            opacity: isInView ? 1 : 0,
+            transition: "all 0.9s cubic-bezier(.17,.67,.84,.25) 1s",
+          }}
+          className="identifying-facts__data2"
+        >
+          {fact2}
+        </div>
+        <div
+          style={{
+            transform: isInView ? "none" : "translateX(-200px)",
+            opacity: isInView ? 1 : 0,
+            transition: "all 0.9s cubic-bezier(.17,.67,.84,.25) 1.5s",
+          }}
+          className="identifying-facts__data3"
+        >
+          {fact3}
+        </div>
       </div>
     </section>
   );
 };
 
-export default identifyingFacts;
+export default IdentifyingFacts;
